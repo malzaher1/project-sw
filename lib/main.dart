91b45480d735257a12bb3b6294b62e75d9965689
malzaher1 <92646440+malzaher1@users.noreply.github.com>
2025-05-01 16:58:28 +0300

    setup login firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:project/views/screens/auth/login_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+// void main() {
+//   runApp(const MyApp());
+// }
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+      await Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform,
+      );
+      runApp(const MyApp());}
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
