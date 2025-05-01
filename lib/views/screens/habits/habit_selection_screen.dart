a4fa168873ea385969ba7267f05b6a45f33f14f9
malzaher1 <92646440+malzaher1@users.noreply.github.com>
2025-05-01 21:04:47 +0300

    small changes

--- lib/views/screens/habits/habit_selection_screen.dart
+++ lib/views/screens/habits/habit_selection_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:project/services/habit_service.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class HabitSelectionScreen extends StatefulWidget {
   @override
@@ -15,7 +16,17 @@ class _HabitSelectionScreenState extends State<HabitSelectionScreen> {
     'Educational': ['Read a book for 30 minutes', 'Learn a new word daily'],
   };
 
-  // To keep track of selected habits
+  bool _isFirstTime = true;
+
+    Future<void> _checkIfFirstTime() async {
+    final prefs = await SharedPreferences.getInstance();
+    bool hasSelectedHabits = prefs.getBool('hasSelectedInitialHabits') ?? false;
+    setState(() {
+      _isFirstTime = !hasSelectedHabits;
+    });
+  }
+  
+    // To keep track of selected habits
   final Set<String> _selectedHabits = {};
 
   void _toggleHabit(String habit, bool isSelected) {
@@ -45,12 +56,17 @@ class _HabitSelectionScreenState extends State<HabitSelectionScreen> {
         });
 
         await _habitService.saveSelectedHabitsWithCategory(habitsWithCategory); 
-        print('Proceeding with selected habits: $habitsWithCategory');
-        Navigator.pushReplacementNamed(context, '/daily_tracker');
+      if (_isFirstTime) {
+        final prefs = await SharedPreferences.getInstance();
+        await prefs.setBool('hasSelectedInitialHabits', true);
+        Navigator.pushReplacementNamed(context, '/main'); 
       } else {
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(content: Text('Please select at least one habit to continue.')),
-        );
+        Navigator.pushReplacementNamed(context, '/daily_tracker'); 
+      }
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Please select at least one habit to continue.')),
+      );
       }
     }
 
